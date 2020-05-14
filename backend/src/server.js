acdb1fdfa442a742fe122615edcbe3e937b7adfa
EduhDev <eduardocarvalho970@gmail.com>
2020-05-14 00:34:42 -0300

    Backend changed to deploy on Heroku

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,37 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
-const mongooseKey = require("../credentials/universal_key.json").key;
 const cors = require("cors");
 
 const routes = require("./routes");
 
-const server = express();
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
-mongoose.connect(
-  `mongodb+srv://eduhdev:${mongooseKey}@cluster0-hsic3.mongodb.net/tindev?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
+const connectedUsers = {};
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+io.on("connection", (socket) => {
+  const { user } = socket.handshake.query;
 
-server.listen(3333);
+  connectedUsers[user] = socket.id;
+});
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(process.env.PORT || 3333);
